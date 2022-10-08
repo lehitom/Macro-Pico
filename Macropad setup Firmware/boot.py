4975ef573f7b5d2288b7a7675f37589810305064
lehitom <48111403+lehitom@users.noreply.github.com>
2022-10-07 20:19:52 -0600

    Built a test firmware for the pico

---
+++ Macropad setup Firmware/boot.py
@@ -0,0 +1,23 @@
+import supervisor
+import board
+import storage
+import usb_cdc
+
+from digitalio import DigitalInOut, Direction, Pull
+
+supervisor.set_next_stack_limit(4096 + 4096)
+
+row = DigitalInOut(board.GP3)
+col = DigitalInOut(board.GP4)
+led = DigitalInOut(board.LED)
+
+row.direction = Direction.INPUT
+col.direction = Direction.OUTPUT
+
+row.pull = Pull.DOWN
+col.value = True
+
+if not row.value:
+    #storage.disable_usb_drive()
+    #usb_cdc.disable()
+    led.value = True
